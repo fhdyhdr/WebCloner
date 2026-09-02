3c1fab5e3193bce29c18f7ce4f2595324d84b02f
fhdyhdr <triasmara.dev@gmail.com>
2026-09-02 17:49:49 +0700

    fix: remove custom start.ts and server.ts to use TanStack Start defaults

--- src/start.ts
+++
@@ -1,6 +0,0 @@
-import { createStart } from "@tanstack/react-start";
-import { renderErrorPage } from "./lib/error-page";
-
-export const startInstance = createStart({
-  requestMiddleware: [],
-});
